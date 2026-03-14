71c4c9ca418b3111939a054e9a8ad3dc89ac3214
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-14 20:15:55 +0700

    update prettier

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -7,26 +7,26 @@ const NODE_ENV = process.env.NODE_ENV ?? 'development';
 const BASE_PATH = NODE_ENV === 'development' ? '' : '/eyes';
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Eyes</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-      <div className={`${geistSans.className} ${geistMono.className}`}>
-        <Component {...pageProps} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Eyes</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="manifest" href="/manifest.json" />
+			</Head>
+			<div className={`${geistSans.className} ${geistMono.className}`}>
+				<Component {...pageProps} />
+			</div>
+		</>
+	);
 }
